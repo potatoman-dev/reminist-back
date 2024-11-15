d05193e6ca61d25819bd63fc81422b5115a03e36
potatoman-dev <enakatani122@gmail.com>
2024-11-15 21:09:20 +0900

    疎通確認用ファイルの削除、TOPへメッセージ表示

--- db/migrate/20241115042035_create_connectivity_tests.rb
+++
@@ -1,9 +0,0 @@
-class CreateConnectivityTests < ActiveRecord::Migration[7.1]
-  def change
-    create_table :connectivity_tests do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
