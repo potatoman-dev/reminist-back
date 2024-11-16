0f9b57a00af493c5066f9a113f53a972d0d58085
potatoman-dev <enakatani122@gmail.com>
2024-11-16 16:07:44 +0900

    Add: devise

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,14 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       root "home#index"
+
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+        registrations: 'api/v1/auth/registrations',
+      }
+
+      namespace :auth do
+        resources :sessions, only: [:index]
+      end
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
