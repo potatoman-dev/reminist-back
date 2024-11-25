233dc7a1b64cd6ce02422d8f0fa574e5bd9e8019
potatoman-dev <enakatani122@gmail.com>
2024-11-25 17:11:11 +0900

    Update entrypoint script to run database migrations in production environment

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 rm -f /app/tmp/pids/server.pid
 
 if [ "$RAILS_ENV" = "production" ]; then
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:connection:close db:drop db:setup
+bundle exec rails db:migrate
 fi
 
 exec "$@"
