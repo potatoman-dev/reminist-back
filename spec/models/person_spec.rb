cfc0917f452b8519186d67dcf7b5450eaa0cd037
potatoman-dev <enakatani122@gmail.com>
2024-11-26 13:00:38 +0900

    Add person update test

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -24,4 +24,21 @@ RSpec.describe Person, type: :model do
       expect(person.errors[:name]).to include("は100文字以内で入力してください")
     end
   end
+
+  describe "update" do
+    let(:user) { build(:user) }
+    let(:person) { build(:person, user: user) }
+
+    it "ヒトを正常に編集できる" do
+      person.update(name: "ヒトノスケ_updated")
+      expect(person).to be_valid
+      expect(person.errors).to be_empty
+    end
+
+    it "ヒトの編集に失敗する" do
+      person.update(name: "")
+      expect(person).to be_invalid
+      expect(person.errors[:name]).to include("を入力してください")
+    end
+  end
 end
