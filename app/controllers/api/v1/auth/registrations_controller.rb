64ecc7194d5ad0561f53d73e7506d39d42e0438f
potatoman-dev <enakatani122@gmail.com>
2024-12-29 19:19:11 +0900

    feat: add name update feature to registrations controller

--- app/controllers/api/v1/auth/registrations_controller.rb
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -1,7 +1,23 @@
 class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
+  before_action :configure_permitted_parameters
+
+  protected
+
+    def update_resource(resource, params)
+      resource.update_without_password(params)
+    end
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    end
+
   private
 
     def sign_up_params
       params.permit(:email, :password, :password_confirmation, :name)
+    end
+
+    def account_update_params
+      params.permit(:name)
     end
 end
